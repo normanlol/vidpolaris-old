55ffd554b00abed1b0e75fe2418f4824a8c3fb2f
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-05-27 01:30:39 -0400

    updated presets

--- js/app.js
+++ js/app.js
@@ -3656,12 +3656,14 @@ function preset(d) {
 			document.getElementById("autoComm").value = "n";
 			document.getElementById("disableCards").value = "y";
 			document.getElementById("showReddit").value = "n";
+			document.getElementById("showSize").value = "n";
 		} else if (d == "dataHeavy") {
 			document.getElementById("sq").value = "y";
 			document.getElementById("suggest").value = "y";
 			document.getElementById("autoComm").value = "y";
 			document.getElementById("disableCards").value = "n";
 			document.getElementById("showReddit").value = "y";
+			document.getElementById("showSize").value = "y";
 		} else if (d == "default") {
 			document.getElementById("sq").value = "y";
 			document.getElementById("theme").value = "d_v1";
@@ -3670,7 +3672,8 @@ function preset(d) {
 			document.getElementById("country").value = "us";
 			document.getElementById("autoComm").value = "y";
 			document.getElementById("disableCards").value = "n";
-			document.getElementById("showReddit").value = "y";
+			document.getElementById("showReddit").value = "n";
+			document.getElementById("showSize").value = "n";
 			document.getElementById("aas").value = "y";
 			document.getElementById("invIns").value = "o";
 		}
