0d9dee919677b95813965415583c6732a09a4b09
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-06-12 12:55:46 -0400

    jsond.info bug fix

--- js/app.js
+++ js/app.js
@@ -2225,12 +2225,11 @@ function openVideo(opt,ret) {
 							var option = document.createElement("OPTION");
 							if (localStorage.getItem("showSize")) {
 								if (localStorage.getItem("showSize") == "y") {
-									if (jsond.info.formats[c].contentLength) {
-											option.innerHTML = jsond.info.formats[c].qualityLabel + " (video) - " + jsond.info.formats[c].audioBitrate + "kbps (audio) [" + formatBytes(parseInt(jsond.info.formats[c].contentLength)) + "]";
-												} else {
-													option.innerHTML = jsond.info.formats[c].qualityLabel + " (video) - " + jsond.info.formats[c].audioBitrate + "kbps (audio) [size not avaliable]";
-												}
-									option.innerHTML = jsond.datainfo[c].qualityLabel + " (video) - " + jsond.datainfo[c].audioBitrate + "kbps (audio) [" + formatBytes(parseInt(jsond.datainfo[c].contentLength)) + "]";
+									if (jsond.datainfo[c].contentLength) {
+											option.innerHTML = jsond.datainfo[c].qualityLabel + " (video) - " + jsond.datainfo[c].audioBitrate + "kbps (audio) [" + formatBytes(parseInt(jsond.datainfo[c].contentLength)) + "]";
+										} else {
+											option.innerHTML = jsond.datainfo[c].qualityLabel + " (video) - " + jsond.datainfo[c].audioBitrate + "kbps (audio) [size not avaliable]";	
+										}
 								} else {
 									option.innerHTML = jsond.datainfo[c].qualityLabel + " (video) - " + jsond.datainfo[c].audioBitrate + "kbps (audio) [size not avaliable]";
 								} 
